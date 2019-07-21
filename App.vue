cd3038f8c791a024032559dcd6b01768d8a4b63d
taoli17 <2312247878@qq.com>
2019-07-21 11:42:54 +0800

    6

---
+++ App.vue
@@ -0,0 +1,24 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+	@import './common/uni.css';
+	/* 引入自定义样式 */
+	@import './common/icon.css';
+	/* 引入动画库 */
+	@import './common/animate.css';
+	/* 自定义样式 */
+	@import './common/common.css';
+</style>
